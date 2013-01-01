95c625346a4b1e8d1b404997b985a12bbdb98167
Srinath Ravindran <Srinath@srinath-2.local>
2013-01-01 16:38:45 -0500

    update two-way-dict

--- PyCVSandbox/pyPlanning/twoWayDict.py
+++ PyCVSandbox/pyPlanning/twoWayDict.py
@@ -5,12 +5,3 @@ class TwoWayDict(dict):
     def __setitem__(self, key, value):
         dict.__setitem__(self, key, value)
         dict.__setitem__(self, value, key)
-
-d = TwoWayDict()
-d[(1,2)] = "red"
-d[(1,4)] = "blue"
-d[(1,6)] = "green"
-
-print d[(1,2)], d["red"]
-print d[(1,6)], d["green"]
-print d[(1,4)], d["blue"]
