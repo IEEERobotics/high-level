9d300110d65ad832a2d11269c2af209f804198c3
Daniel Farrell <dfarrell07@gmail.com>
2013-02-25 11:20:54 -0500

    Built setup.py for creating distribution file

---
+++ qwe/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='qwe',
+    version='0.1.0',
+    author='NCSU IEEE Ground Robotics Team',
+    author_email='ncsuhardware@googlegroups.com',
+    packages=['vision', 'sensors', 'comm', 'localizer', 'mapping', 'action', 'navigation', 'planning'],
+    scripts=[],
+    url='https://github.com/NCSUhardware/high-level',
+    license='LICENSE.txt',
+    description='IEEE SECon 2013 robot code',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
