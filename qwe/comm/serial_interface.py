3d49e25f51813dc56ba51be0b5a8b0714625807f
Arpan Chakraborty <napratin@yahoo.co.in>
2013-03-07 15:49:44 -0500

    Implemented several functions for serial communication (multi-threaded, using a queue); renamed module from serial.py to serial_interface.py to avoid conflict with Python top-level serial module.

---
+++ qwe/comm/serial_interface.py
@@ -0,0 +1,181 @@
+"""
+Primary communication module to interact with motor and sensor controller board over serial.
+"""
+
+import sys
+import serial
+import threading
+import Queue
+
+class SerialInterface:
+  """
+  Encapsulates functionality to send (multiplexed) commands over a serial line.
+  Exposes a set of methods for different navigation, action and sensor commands.
+  """
+  PORT = "/dev/ttyO3"
+  BAUDRATE = 57600
+  TIMEOUT = 1  # seconds; float allowed
+  
+  QUEUE_MAXSIZE = 10
+  
+  def __init__(self, port=PORT, baudrate=BAUDRATE, timeout=TIMEOUT):
+    self.port = port
+    self.baudrate = baudrate
+    self.timeout = timeout
+    # NOTE Other default port settings: bytesize=8, parity='N', stopbits=1, xonxoff=0, rtscts=0
+    self.device = None  # open serial port in start()
+    
+    self.queue = Queue.Queue(SerialInterface.QUEUE_MAXSIZE)  # create internal queue (multiple priority-level queues?) to receive and service commands
+    self.responses = { }  # create a map structure to store responses by commandId
+    
+    self.loopThread = threading.Thread(target=self.loop)
+    # TODO use multiprocessing and multiprocessing.Queue instead of threading and Queue
+  
+  def start(self):
+    """Open serial port and start main loop thread/process."""
+    try:
+      self.device = serial.Serial(self.port, self.baudrate, timeout=self.timeout)  # open serial port
+    except serial.serialutil.SerialException as e:
+      print "SerialInterface.start(): Error: %s" % e
+      return False
+    
+    if self.device.isOpen():
+      print "SerialInterface.start(): Serial port \"%s\" open" % self.device.name
+    else:
+      print "SerialInterface.start(): Unspecified error opening serial port \"%s\"" % self.port
+      return False
+    
+    self.loopThread.start()
+    
+    return True
+  
+  def loop(self):
+    """Main loop: Monitor queue for commands and service them until signaled to quit."""
+    print "SerialInterface.loop(): Starting main [LOOP]..."
+    while True:
+      try:
+        (commandId, command) = self.queue.get(True)  # blocks indefinitely
+        if command == "quit":  # special "quit" command breaks out of loop
+          break
+        #print "[LOOP] Command : " + command
+        
+        response = self.execCommand(command)
+        if response is None:  # None response means something went wrong, break out of loop
+          break
+        #print "[LOOP] Response: " + response
+        
+        self.responses[commandId] = command  # store result by commandId for later retrieval
+      except Queue.Empty:
+        print "[LOOP] Empty queue"
+        pass  # if queue is empty, simply loop back and wait for more commands
+    
+    # Clean up: Close serial port
+    print "SerialInterface.loop(): Main loop terminated"
+    if self.device is not None and self.device.isOpen():
+      self.device.close()
+      print "SerialInterface.loop(): Serial port closed"
+    
+    # Clean up: Clear queue (print warning if there are unserviced commands?)
+    if not self.queue.empty():
+      print "SerialInterface.loop(): Warning: Terminated with pending commands"
+    self.queue = None  # NOTE cannot call putCommand after this
+    # TODO find a better way to simply clear the queue (get until empty?)
+    self.responses.clear()
+  
+  def stop(self):
+    """Stop main loop by sending quit command."""
+    self.putCommand("quit")
+  
+  def execCommand(self, command):
+    """Execute command (send over serial port) and return response"""
+    try:
+      self.device.write(command + "\n")  # '\n' terminated command
+      response = self.device.readline()  # '\n' terminated response
+      return response
+    except Exception as e:
+      print "SerialInterface.execCommand(): Error: " + e
+      return None
+  
+  def putCommand(self, command, priority=0):
+    commandId = 0
+    self.queue.put((commandId, command))  # queue items are 2-tuples
+    return commandId  # TODO insert command into internal queue (by priority?) and return a commandId (e.g. a generated random number)
+  
+  def getResponse(self, commandId, block=True):
+    if block:
+      while not commandId in self.responses:
+        pass  # if blocking, wait till command has been serviced
+    elif not commandId in self.responses:
+      return None  # if non-blocking and command hasn't been serviced, return None
+    
+    response = self.responses.pop(commandId)  # get response and remove it
+    return response
+  
+  def botMove(self, distance):
+    pass  # TODO send move command, wait for completion ack, return actual distance traveled (relative)
+  
+  def botTurn(self, angle):
+    pass  # TODO send turn command, wait for completion ack, return actual angle turned (relative)
+  
+  def armRotate(self, angle):
+    pass  # TODO send arm rotate command, wait for completion ack, return actual arm angle (absolute?)
+  
+  def armDown(self):
+    pass  # TODO rotate arm to lowest position (to pick-up blocks) [use armRotate], return True/False to indicate success/failure
+  
+  def armUp(self):
+    pass  # TODO rotate arm to highest position (e.g. with block in gripper) [use armRotate], return True/False to indicate success/failure
+  
+  def gripperSet(self, value):
+    pass  # TODO open gripper to specified value (distance.angle) and return True/False on completion
+  
+  def gripperClose(self):
+    pass  # TODO close gripper (to grab) [use gripperSet] and return True/False on completion
+  
+  def gripperOpen(self):
+    pass  # TODO open gripper (to release) [use gripperSet] and return True/False on completion
+  
+  def getSensorData(self, sensorId):
+    pass  # TODO obtain data for given sensorId and return value
+
+
+def main():
+  """Method for standalone testing."""
+  port = SerialInterface.PORT
+  baudrate = SerialInterface.BAUDRATE
+  timeout = SerialInterface.TIMEOUT
+  
+  if len(sys.argv) > 1:
+    port = sys.argv[1]
+    if len(sys.argv) > 2:
+      baudrate = int(sys.argv[2])
+      if len(sys.argv) > 3:
+        timeout = None if sys.argv[3] == "None" else float(sys.argv[3])
+  
+  print "main(): Creating SerialInterface(port=\"{port}\", baudrate={baudrate}, timeout={timeout})".format(port=port, baudrate=baudrate, timeout=timeout)
+  serialInterface = SerialInterface(port, baudrate, timeout)
+  if not serialInterface.start():
+    return
+  
+  print "main(): Starting interactive session [Ctrl+D or \"quit\" to end]...\n"
+
+  while True:
+    try:
+      command = raw_input("Me    : ")  # input command from user
+    except EOFError:
+      command = "quit"
+    
+    commandId = serialInterface.putCommand(command)
+    if command == "quit":
+      print "\nmain(): Exiting interactive session..."
+      break
+    
+    response = serialInterface.getResponse(commandId)
+    print "Device: " + response
+  
+  #serialInterface.stop()
+  print "main(): Done."
+
+
+if __name__ == "__main__":
+  main()
