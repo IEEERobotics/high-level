93d352958495ab4ed0f0be763d2668246d6362ec
Daniel Farrell <dfarrell07@gmail.com>
2013-03-09 17:57:01 -0500

    Minor tweaks

--- qwe/navigation/nav.py
+++ qwe/navigation/nav.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 """Primary module of navigation package"""
 
-from comm import serial_interface as comm
+#from comm import serial_interface as comm
+import comm.serial_interface as comm
 
 def run(bot_loc, blocks, zones, corners, course_map, waypoints):
   print "Nothing to see here"
 
 def moveForward(distance, speed):
-  """Very stupid function for moving foward. Goal is to get started with message passing between planner and comm."""
+  """Very stupid function for moving forward. Goal is to get started with message passing between planner and comm."""
   si = comm.SerialInterface()
   actual_move = si.botMove(distance, speed)
   return actual_move
