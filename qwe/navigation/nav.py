3d6f2557498abd2883b047bea76570927520f3ab
Daniel Farrell <dfarrell07@gmail.com>
2013-03-09 17:49:51 -0500

    Added stupid fn for moving forward. Goal is to start basic message passing work.

--- qwe/navigation/nav.py
+++ qwe/navigation/nav.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 """Primary module of navigation package"""
 
+from comm import serial_interface as comm
+
 def run(bot_loc, blocks, zones, corners, course_map, waypoints):
   print "Nothing to see here"
+
+def moveForward(distance, speed):
+  """Very stupid function for moving foward. Goal is to get started with message passing between planner and comm."""
+  si = comm.SerialInterface()
+  actual_move = si.botMove(distance, speed)
+  return actual_move
