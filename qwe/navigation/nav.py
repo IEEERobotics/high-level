737b7f951ee13506d3b9bdbe2e10f9f48155a7b5
Daniel Farrell <dfarrell07@gmail.com>
2013-03-23 18:35:18 -0400

    Fixed test env file generated by nav.getEnvTestFile such that it diffs correctly with SBPL's version

--- qwe/navigation/nav.py
+++ qwe/navigation/nav.py
@@ -23,21 +23,21 @@ CELLSIZE = 0.025 # Meters
 NOMINALVEL = 1.0 # Meters per second
 TIMETOTURN45DEGSINPLACE = 2.0 # Seconds
 ENVIROMENT = \
-"""0 0 0 0 0 0 1 1 0 0 0 0 0 0 0  
-0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 
-0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 
-0 0 0 0 0 0 1 1 1 1 0 0 0 0 0 
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
-0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 
-0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 
-1 1 1 1 0 0 0 0 0 0 0 1 0 0 0 
-1 1 1 1 0 0 0 0 0 0 0 1 0 0 0 
-0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 
-0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 
-0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 
-0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 
-0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 
-0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 """
+"0 0 0 0 0 0 1 1 0 0 0 0 0 0 0  " + "\r\n" \
++ "0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 " + "\r\n" \
++ "0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 " + "\r\n" \
++ "0 0 0 0 0 0 1 1 1 1 0 0 0 0 0 " + "\r\n" \
++ "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 " + "\r\n" \
++ "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 " + "\r\n" \
++ "0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 " + "\r\n" \
++ "1 1 1 1 0 0 0 0 0 0 0 1 0 0 0 " + "\r\n" \
++ "1 1 1 1 0 0 0 0 0 0 0 1 0 0 0 " + "\r\n" \
++ "0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 " + "\r\n" \
++ "0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 " + "\r\n" \
++ "0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 " + "\r\n" \
++ "0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 " + "\r\n" \
++ "0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 " + "\r\n" \
++ "0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 " + "\r\n"
 
 def run(bot_loc, blocks, zones, corners, course_map, waypoints):
   """Function that accepts initial data from controller and kicks off nav. Will eventually involve instaniating an class.
@@ -63,25 +63,25 @@ def moveForward(distance, speed):
   actual_move = si.botMove(distance, speed)
   return actual_move
 
-def genEnvFile(currentPose = {'theta': 0, 'x': 0.11, 'y': 0.11}, goalPose = {'theta': 0, 'x': 0.35, 'y': 0.3}):
+def genEnvTestFile(currentPose = {'theta': 0, 'x': 0.11, 'y': 0.11}, goalPose = {'theta': 0, 'x': 0.35, 'y': 0.3}):
   """Builds a file suitable as input for SBPL. Eventually, params will be passed directly to SBPL, not written out.
 
   :param goalPose: X, Y and theta of the desired bot location
   """
   # Open enviroment file for writing
-  envFile = open("navigation/envFile.cfg", "w")
+  envFile = open("navigation/envs/envTestFile.cfg", "w")
 
   # Write configuration to environment file
-  envFile.write("discretization(cells): " + str(DISCRETIZATION) + " " + str(DISCRETIZATION) + "\n"
-    + "obsthresh: " + str(OBSTHRESH) + "\n"
-    + "cost_inscribed_thresh: " + str(COST_INSCRIBED_THRESH) + "\n"
-    + "cost_possibly_circumscribed_thresh: " + str(COST_POSSIBLY_CIRCUMSCRIBED_THRESH) + "\n"
-    + "cellsize(meters): " + str(CELLSIZE) + "\n"
-    + "nominalvel(mpersecs): " + str(NOMINALVEL) + "\n"
-    + "timetoturn45degsinplace(secs): " + str(TIMETOTURN45DEGSINPLACE) + "\n"
-    + "start(meters,rads): " + str(currentPose["x"]) + " " + str(currentPose["y"]) + " " + str(currentPose["theta"]) + "\n"
-    + "end(meters,rads): " + str(goalPose["x"]) + " " + str(goalPose["y"]) + " " + str(goalPose["theta"]) + "\n"
-    + "environment:" + "\n" + str(ENVIROMENT))
+  envFile.write("discretization(cells): " + str(DISCRETIZATION) + " " + str(DISCRETIZATION) + "\r\n"
+    + "obsthresh: " + str(OBSTHRESH) + "\r\n"
+    + "cost_inscribed_thresh: " + str(COST_INSCRIBED_THRESH) + "\r\n"
+    + "cost_possibly_circumscribed_thresh: " + str(COST_POSSIBLY_CIRCUMSCRIBED_THRESH) + "\r\n"
+    + "cellsize(meters): " + str(CELLSIZE) + "\r\n"
+    + "nominalvel(mpersecs): " + str(NOMINALVEL) + "\r\n"
+    + "timetoturn45degsinplace(secs): " + str(TIMETOTURN45DEGSINPLACE) + "\r\n"
+    + "start(meters,rads): " + str(currentPose["x"]) + " " + str(currentPose["y"]) + " " + str(currentPose["theta"]) + "\r\n"
+    + "end(meters,rads): " + str(goalPose["x"]) + " " + str(goalPose["y"]) + " " + str(goalPose["theta"]) + "\r\n"
+    + "environment:" + "\r\n" + str(ENVIROMENT))
 
   # Close environment file
   envFile.close()
