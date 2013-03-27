bde6417136d61f9c6353bb3ada605eb50f51d7b6
Arpan Chakraborty <napratin@yahoo.co.in>
2013-03-27 19:44:11 -0400

    Made changes to controller to use serial_interface properly after updates there. Also added a shared blobs list between vision and planner.

--- qwe/controller.py
+++ qwe/controller.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
   logger.debug("Logger is set up")
 
   # Start serial communication to low-level board
-  si = comm.SerialInterface()
+  si = comm.SerialInterface()  # TODO create shared structures commands and responses here and pass on to si? currently it creates them internally)
   si.start() # Displays an error if port not found (not running on Pandaboard)
   logger.info("Serial interface set up")
 
@@ -42,6 +42,7 @@ if __name__ == "__main__":
   # See the following for details: http://goo.gl/SNNAs
   manager = Manager()
   bot_loc = manager.dict(x=None, y=None, theta=None)
+  blobs = manager.list()  # for communication between vision and planner
   blocks = manager.list()
   zones = manager.list()
   corners = manager.list()
@@ -62,22 +63,27 @@ if __name__ == "__main__":
   logger.info("Map properties unpickled")
 
   # Start planner process, pass it shared data
-  pPlanner = Process(target=planner.run, args=(bot_loc, blocks, zones, waypoints, si, bot_state, qMove_nav))
+  scPlanner = comm.SerialCommand(si.commands, si.responses)  # create one SerialCommand wrapper for each client
+  # NOTE si.commands and si.responses are process-safe shared structures
+  pPlanner = Process(target=planner.run, args=(bot_loc, blobs, blocks, zones, waypoints, scPlanner, bot_state, qMove_nav))
   pPlanner.start()
   logger.info("Planner process started")
 
   # Start vision process, pass it shared data
-  pVision = Process(target=vision.run, args=(bot_loc, blocks, zones, corners, waypoints, si, bot_state))
+  scVision = comm.SerialCommand(si.commands, si.responses)
+  pVision = Process(target=vision.run, args=(bot_loc, blobs, blocks, zones, corners, waypoints, scVision, bot_state))
   pVision.start()
   logger.info("Vision process started")
 
   # Start navigator process, pass it shared data
-  pNav = Process(target=nav.run, args=(bot_loc, course_map, waypoints, qNav_loc, si, bot_state, qMove_nav))
+  scNav = comm.SerialCommand(si.commands, si.responses)
+  pNav = Process(target=nav.run, args=(bot_loc, course_map, waypoints, qNav_loc, scNav, bot_state, qMove_nav))
   pNav.start()
   logger.info("Navigator process started")
 
   # Start localizer process, pass it shared data, waypoints, map_properties course_map and queue for talking to nav
-  pLocalizer = Process(target=localizer.run, args=(bot_loc, blocks, corners, map_properties, course_map, qNav_loc, si, bot_state))
+  scLocalizer = comm.SerialCommand(si.commands, si.responses)
+  pLocalizer = Process(target=localizer.run, args=(bot_loc, blocks, corners, map_properties, course_map, qNav_loc, scLocalizer, bot_state))
   pLocalizer.start()
   logger.info("Localizer process started")
 
@@ -89,3 +95,7 @@ if __name__ == "__main__":
   logger.info("Joined localizer process")
   pPlanner.join()
   logger.info("Joined planner process")
+  
+  scPlanner.quit()
+  si.join()
+  logger.info("Joined SerialInterface process")
