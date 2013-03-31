025407d3b07642e29133161e6e728dc787fd5473
Jeff Schornick <jeff@schornick.org>
2013-03-31 00:38:21 -0400

    Mapping: fillLoc takes waypoints param, fix res bug
    - the waypoints structure isn't in fillLoc's scope, so pass it in as a parameter
    - map_grid_vars doesn't exist in mvars, so calculate them on the fly using res

--- qwe/mapping/map_class.py
+++ qwe/mapping/map_class.py
@@ -9,10 +9,13 @@ class MapClass:
 		self.scale = res
 		# make a numpy 2D array
 		self.grid = np.zeros(mapSize, dtype = [('desc', np.uint8),('status', '|S1'),('color', '|S1'),('level', '|S1'),('path', '|S1')])
+
 	def ydim(self): 	#return the y dimension of grid (number of rows)
 		return(len(self.grid))
+
 	def xdim(self):		#return the x dimension of grid (number of columns)
 		return(len(self.grid[0]))	
+
 	def fillCoords(self, p1, p2, prop):	#fill an area defined by p1 and p2 with value prop['key'] in layer key of MyMap.grid
 		xd = self.xdim()			#calc number of rows
 		yd = self.ydim()			#calc number of columns
@@ -34,28 +37,30 @@ class MapClass:
 			for x in xrange(x_range[0],x_range[1]):
 				for key in prop:
 					self.grid[y][x][key] = prop[key]
-	def fillLoc(self, key, prop):	#fill a location in a layer with a value (both given by prop dict).  The location to be filled is identified by key (key comes from waypoints['key'], i.e. key could be "L01")
-		x = waypoints[key][0][0]	# get x and y coords of location
-		y = waypoints[key][0][1]
+
+	def fillLoc(self, waypoints, key, prop):	#fill a location in a layer with a value (both given by prop dict).  The location to be filled is identified by key (key comes from waypoints['key'], i.e. key could be "L01")
+                x = waypoints[key][0][0]	# get x and y coords of location
+                y = waypoints[key][0][1]
+                res = self.res
 		if key[0] is "L": 			#key is a land location
 			
 			x1 = int(x + 1.5/2*res)		#blocks are 1.5 in wide
-			y1 = int(y - mvars.map_grid_vars['offset'] - mvars.map_grid_vars['whiteLine'])
+			y1 = int(y - res * mvars.map_real_vars['offset'] - res * mvars.map_real_vars['whiteLine'])
 			x2 = int(x - 1.5/2*res)
 			y2 = int(y1 - 4*res +1)		#land blocks are 4 in long
 		elif key[0] is "A":			#Air
-			x1 = int(x - mvars.map_grid_vars['offset']- mvars.map_grid_vars['whiteLine'])
+			x1 = int(x - res * mvars.map_real_vars['offset']- res * mvars.map_real_vars['whiteLine'])
 			y1 = int(y + 1.5/2*res) 	#blocks are 1.5 in wide
 			x2 = int(x1 - 2*res +1)	#air blocks are 2 in long
 			y2 = int(y - 1.5/2*res)
 		elif key[1] is "e":			#Sea
-			x1 = int(x - mvars.map_grid_vars['offset']- mvars.map_grid_vars['whiteLine'])
+			x1 = int(x - res * mvars.map_real_vars['offset']- res * mvars.map_real_vars['whiteLine'])
 			y1 = int(y + 1.5/2*res) 	#blocks are 1.5 in wide
 			x2 = int(x1 - 3*res +1)	#sea blocks are 3 in long
 			y2 = int(y - 1.5/2*res)
 		else:					#storage
 			x1 = int(x + 1.5/2*res)		#blocks are 1.5 in wide
-			y1 = int(y + mvars.map_grid_vars['offset'] + mvars.map_grid_vars['whiteLine'])
+			y1 = int(y + res * mvars.map_real_vars['offset'] + res * mvars.map_real_vars['whiteLine'])
 			x2 = int(x - 1.5/2*res)
 			y2 = int(y1 + 4*res - 1)		#use all land blocks to fill storage
 		#prepare points to send to fillCoords
