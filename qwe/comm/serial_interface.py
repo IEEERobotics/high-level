a5d2d15c3ae73bac12321ced702fd4f632ab144d
Arpan Chakraborty <napratin@yahoo.co.in>
2013-04-03 10:50:32 -0400

    Added signal handler to comm, fixed fake send-recv issue.

--- qwe/comm/serial_interface.py
+++ qwe/comm/serial_interface.py
@@ -3,6 +3,7 @@ Primary communication module to interact with motor and sensor controller board 
 """
 
 import sys
+import signal
 import json
 import random
 import serial
@@ -30,9 +31,9 @@ servo_delay = 1.0  # secs.; duration to sleep after sending a servo command to l
 fake_delay = 0.001  # secs.; duration to sleep for when faking serial comm.
 
 # TODO use direct commands [left, right]_[up, down, open, close, grab, drop]
-Arm = namedtuple('Arm', ['arm_id', 'arm_angles', 'gripper_id', 'gripper_angles'])
-left_arm = Arm(arm_id=0, arm_angles=(680, 310), gripper_id=1, gripper_angles=(900, 450))
-right_arm = Arm(arm_id=2, arm_angles=(330, 710), gripper_id=3, gripper_angles=(0, 350))
+Arm = namedtuple('Arm', ['name', 'arm_id', 'arm_angles', 'gripper_id', 'gripper_angles'])
+left_arm = Arm("left", arm_id=0, arm_angles=(680, 310), gripper_id=1, gripper_angles=(900, 450))
+right_arm = Arm("right", arm_id=2, arm_angles=(330, 710), gripper_id=3, gripper_angles=(0, 350))
 
 sensors = { "heading": 0,  # compass / magnetometer
             "accel.x": 1,
@@ -55,6 +56,7 @@ class SerialInterface(Process):
     # NOTE Other default port settings: bytesize=8, parity='N', stopbits=1, xonxoff=0, rtscts=0
     self.device = None  # open serial port in run()
     self.live = False  # flag to signal threads
+    self.fake_id = -1  # temp id used to make fake send-recv work
     
     # Create data structures to store commands and responses, unless passed in
     #self.sendLock = Lock()  # to prevent multiple processes from trying to send on the same serial line
@@ -91,6 +93,10 @@ class SerialInterface(Process):
       self.send = self.fakeSend
       self.recv = self.fakeRecv
     
+    # Set signal handler before starting communication loop (NOTE must be done in the main thread of this process)
+    signal.signal(signal.SIGTERM, self.handleSignal)
+    signal.signal(signal.SIGINT, self.handleSignal)
+    
     if(is_sequential):
       # Start sequential execution (send + receive) thread
       print "SerialInterface.run(): Starting exec thread..."
@@ -118,15 +124,25 @@ class SerialInterface(Process):
       self.recvThread.join()
       print "SerialInterface.run(): Send and receive threads joined."
     
+    # Reset signal handlers to default behavior
+    signal.signal(signal.SIGTERM, signal.SIG_DFL)
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    
     # Clean up: Clear queue; print warning if there are unserviced commands
     if not self.commands.empty():
-      print "SerialInterface.run(): Warning: Terminated with pending command(s)"
-      # TODO clear the queue (get items until empty?)
+      print "SerialInterface.run(): Warning: Terminated with pending command(s):"
+      while not self.commands.empty():
+        try:
+          id, command = self.commands.get(False)  # do not block, in case some other thread/process has consumed commands in the meantime
+          print "{id} {command}".format(id=id, command=command)
+          # NOTE this also clears the queue (gets items until empty)
+        except Queue_Empty:
+          break
     self.commands = None
     
     # Clean up: Clear responses dict; print warning if there are unfetched responses
     if self.responses:
-      print "SerialInterface.run(): Warning: Terminated with unfetched response(s)"
+      print "SerialInterface.run(): Warning: Terminated with unfetched response(s):", ", ".join((str(id) + ": " + str(response) for id, response in self.responses.iteritems()))
       self.responses.clear()
     self.responses = None
     
@@ -135,64 +151,84 @@ class SerialInterface(Process):
       self.device.close()
       print "SerialInterface.run(): Serial port closed"
   
+  def quit(self):
+    self.commands.put((-1, "quit"))
+  
+  # TODO debug this
+  def handleSignal(self, signum, frame):
+    if signum == signal.SIGTERM or signum == signal.SIGINT:
+      print "SerialInterface.handleSignal(): Termination signal ({0}); stopping comm loop...".format(signum)
+    else:
+      print "SerialInterface.handleSignal(): Unknown signal ({0}); stopping comm loop anyways...".format(signum)
+    self.quit()
+  
   def sendLoop(self, block=True):
     """Monitor queue for commands and send them until signaled to quit."""
-    print "SerialInterface.sendLoop(): [SEND] loop starting..."
+    print "SerialInterface.sendLoop(): [SEND-LOOP] Starting..."
     while self.live:
       try:
         (id, command) = self.commands.get(block)  # block=True waits indefinitely for next command
         if command == "quit":  # special "quit" command breaks out of loop
           self.live = False  # signal any other threads to quit as well
           break
-        #print "[SEND] Command :", command
+        #print "[SEND-LOOP] Command :", command  # [debug]
         self.send(id, command)
       except Queue_Empty:
-        print "[SEND] Empty queue"
-        pass  # if queue is empty (after timeout), simply loop back and wait for more commands
-    print "SerialInterface.sendLoop(): [SEND] loop terminated."
+        print "[SEND-LOOP] Warning: Empty queue (timeout?)"
+        pass  # if queue is empty (after timeout, e.g.), simply loop back and wait for more commands
+    print "SerialInterface.sendLoop(): [SEND-LOOP] Terminated."
   
   def recvLoop(self):
     """Listen for responses and collect them in internal dict."""
-    
-    print "SerialInterface.recvLoop(): [RECV] loop starting..."
+    print "SerialInterface.recvLoop(): [RECV-LOOP] Starting..."
     while self.live:
       try:
         response = self.recv()
         if response is None:  # None response means something went wrong, break out of loop
-          print "[RECV] No response"
+          print "[RECV-LOOP] Error: None response"
           break
-        else:
-          #print "[RECV] Response:", response
+        elif response:  # if response is not blank
+          #print "[RECV-LOOP] Response:", response  # [debug]
           self.responses[response.get('id', -1)] = response  # store response by id for later retrieval, default id: -1
       except Exception as e:
-        print "[RECV] Error:", e
-    print "SerialInterface.sendLoop(): [RECV] loop terminated."
+        print "[RECV-LOOP] Error:", e
+        break  # something wrong, break out of loop
+    print "SerialInterface.recvLoop(): [RECV-LOOP] Terminated."
   
   def execLoop(self, block=True):
     """Combine functions of sendLoop() and recvLoop() to enforce sequential command execution."""
-    print "SerialInterface.execLoop(): [EXEC] loop starting..."
+    print "SerialInterface.execLoop(): [EXEC-LOOP] Starting..."
     while self.live:
       try:
         (id, command) = self.commands.get(block)  # block=True waits indefinitely for next command
         if command == "quit":  # special "quit" command breaks out of loop
           self.live = False  # signal any other threads to quit as well
           break
-        self.execute(id, command)
+        response = self.execute(id, command)
+        if response is None:  # None response means something went wrong, break out of loop
+          print "[EXEC-LOOP] Error: None response"
+          break
+        elif response:  # if response is not blank
+          #print "[EXEC-LOOP] Response:", response  # [debug]
+          self.responses[response.get('id', id)] = response  # store response by id for later retrieval, default id: what was passed in
       except Queue_Empty:
-        print "[EXEC] Empty queue"
-        pass  # if queue is empty (after timeout), simply loop back and wait for more commands
-    print "SerialInterface.execLoop(): [EXEC] loop terminated."
+        print "[EXEC-LOOP] Warning: Empty queue (timeout?)"
+        pass  # if queue is empty (after timeout, e.g.), simply loop back and wait for more commands
+      except Exception as e:
+        print "[EXEC-LOOP] Error:", e
+        break  # something wrong, break out of loop
+    print "SerialInterface.execLoop(): [EXEC-LOOP] Terminated."
   
   def send(self, id, command):
     """Send a command, adding terminating EOL char(s)."""
     try:
       if prefix_id:
         command = str(id) + ' ' + command
-      print "SerialInterface.send(): Sending: \"{command}\"".format(command=command)
+      print "[SEND] {command}".format(command=command)  # [debug]
       self.device.write(command + command_eol)  # add EOL char(s)
       return True
     except Exception as e:
-      print "SerialInterface.send(): Error:", e
+      print "[SEND] Error:", e
       return False
   
   def recv(self):
@@ -200,39 +236,51 @@ class SerialInterface(Process):
     try:
       responseStr = self.device.readline()  # NOTE response must be \n terminated
       responseStr = responseStr.strip()  # strip EOL
-      print "SerialInterface.recv(): Recvd: \"{0}\"".format(responseStr)
+      print "[RECV] {0}".format(responseStr)  # [debug]
       if len(responseStr) == 0:
-        print "SerialInterface.recv(): Warning: Blank response (timeout?)"
+        print "[RECV] Warning: Blank response (timeout?)"
         return { }  # return a blank dict
       else:
         response = json.loads(responseStr)
         return response  # return dict representation of JSON object
     except Exception as e:
-      print "SerialInterface.recv(): Error:", e
+      print "[RECV] Error:", e
       return None
   
   def execute(self, id, command):
-    """Send a command, wait for response and store it in dict."""
+    """Send a command, wait for response and return it."""
     try:
       self.send(id, command)
       response = self.recv()
-      if response is None:  # None response means something went wrong
-        print "[EXEC] No response"
-      else:
-        self.responses[response.get('id', id)] = response  # store response by id for later retrieval, default id: as passed in
+      #print "[EXEC] {0}".format(response)  # [debug]
+      while response is not None and not response and self.live:
+        response = self.recv()  # wait till a non-None, non-blank-dict response is received; TODO wait only a fixed number of times?
+      return response
     except Exception as e:
       print "[EXEC] Error:", e
+      return None
   
   def fakeSend(self, id, command):
     if prefix_id:
       command = str(id) + ' ' + command
+      self.fake_id = id
     print "[FAKE-SEND] {command}".format(command=command)
     sleep(fake_delay)
     return True
   
   def fakeRecv(self):
+    ctr = 0
+    while self.fake_id == -1 and ctr < 10:
+      sleep(self.timeout / 10)
+      ctr = ctr + 1
+    
+    if self.fake_id == -1:
+      print "[FAKE-RECV] Warning: Blank response (timeout?)"
+      return { }
+    
     sleep(fake_delay)
-    response = { 'result': True, 'msg': "" }
+    response = { 'result': True, 'msg': "", 'id': self.fake_id }  # put in the last id that fakeSend() got
+    self.fake_id = -1
     print "[FAKE-RECV] {response}".format(response=response)
     return response
 
@@ -274,15 +322,20 @@ class SerialCommand:
   
   def botStop(self):
     """Stop immediately."""
-    response = self.runCommand("stop")  # TODO use putCommand() to make "stop" an immediate command (with no response)
+    response = self.runCommand("stop")
     return response.get('result', False)
   
-  def botSetSpeed(self, left, right):
+  def botPWMDrive(self, left, right):
     """Set individual wheel/side speeds (units: PWM values 0 - 10000)."""
-    response = self.runCommand("pwm_drive {left} {right}".format(left=left, right=right))  # TODO use putCommand() [see botStop()]
+    response = self.runCommand("pwm_drive {left} {right}".format(left=left, right=right))
     return response.get('result', False)
+    
+  def botSet(self, distance, angle, speed=default_speed):  # distance: mm (?), angle: 10ths of a degree, speed: encoder units (200-1000, default: 400)
+    """Move distance and turn to given absolute angle simultaneously."""
+    response = self.runCommand("set {angle} {speed} {distance}".format(angle=angle, speed=speed, distance=distance))
+    return int(response.get('distance', distance)), int(response.get('absHeading', angle))  # return 2-tuple (<actual distance>, <abs. heading>)
   
-  def botMove(self, distance, speed=default_speed):  # distance: mm, speed: mm / sec (?)
+  def botMove(self, distance, speed=default_speed):  # distance: mm (?), speed: encoder units (200-1000, default: 400)
     response = self.runCommand("move {speed} {distance}".format(speed=speed, distance=distance))
     return int(response.get('distance', distance))
   
@@ -292,7 +345,7 @@ class SerialCommand:
   
   def botTurnRel(self, angle):  # angle: 10ths of a degree
     response = self.runCommand("turn_rel {angle}".format(angle=int(angle)))
-    return (angle - response.get('headingErr', 0))  # turn_rel returns remaining heading error, i.e. actual - desired; TODO confirm this
+    return (angle - response.get('headingErr', 0))  # turn_rel returns remaining heading error, i.e. desired - actual
   
   def armSetAngle(self, arm_id, angle, ramp=default_arm_ramp):
     response = self.runCommand("servo {channel} {ramp} {angle}".format(channel=arm_id, ramp=ramp, angle=angle))
@@ -300,9 +353,15 @@ class SerialCommand:
     return response.get('result', False)
   
   def armUp(self, arm):
+    # TODO switch to [left/right]_up when ready
+    #response = self.runCommand(arm.name + "_up")
+    #return response.get('result', False)
     return self.armSetAngle(arm.arm_id, arm.arm_angles[0])
   
   def armDown(self, arm):
+    # TODO switch to [left/right]_down when ready
+    #response = self.runCommand(arm.name + "_down")
+    #return response.get('result', False)
     return self.armSetAngle(arm.arm_id, arm.arm_angles[1])
   
   def gripperSetAngle(self, gripper_id, angle, ramp=default_gripper_ramp):
@@ -311,10 +370,18 @@ class SerialCommand:
     return response.get('result', False)
   
   def gripperOpen(self, arm):
+    # TODO switch to [left/right]_open when ready
+    #response = self.runCommand(arm.name + "_open")
+    #return response.get('result', False)
     return self.gripperSetAngle(arm.gripper_id, arm.gripper_angles[0])
   
   def gripperClose(self, arm):
+    # TODO switch to [left/right]_close when ready
+    #response = self.runCommand(arm.name + "_close")
+    #return response.get('result', False)
     return self.gripperSetAngle(arm.gripper_id, arm.gripper_angles[1])
+  
+  # TODO implement armPick() and armDrop()
   
   def getAllSensorData(self):
     return self.runCommand("sensors")  # return the entire dict full of sensor data
@@ -368,6 +435,17 @@ def main():
   # NOTE pass this SerialCommand object to anything that needs to call high-level methods (botMove, botTurn*, getSensorData, etc.)
   sc2 = SerialCommand(si.commands, si.responses) # multiple SerialCommand objects can be created if needed; the underlying SerialInterface data structures are process- and thread- safe
   
+  # Set signal handlers
+  def handleSignal(signum, frame):
+    if signum == signal.SIGTERM or signum == signal.SIGINT:
+      print "handleSignal(): Termination signal ({0}); stopping comm loop...".format(signum)
+    else:
+      print "handleSignal(): Unknown signal ({0}); stopping comm loop anyways...".format(signum)
+    si.quit()
+  
+  signal.signal(signal.SIGTERM, handleSignal)
+  signal.signal(signal.SIGINT, handleSignal)
+  
   # Test sequence, non-interactive
   print "main(): Starting test sequence...\n"
   sc1.botStop()
@@ -379,7 +457,7 @@ def main():
   while True:
     try:
       command = raw_input("Me    > ")  # input command from user
-    except EOFError:
+    except (EOFError, KeyboardInterrupt):
       command = "quit"
     
     if command == "quit":
@@ -391,6 +469,10 @@ def main():
     #response = sc2.getResponse(id)
     print "Device: {response}".format(response=response)
   print "main(): Interactive session terminated."
+  
+  # Reset signal handlers to default behavior
+  signal.signal(signal.SIGTERM, signal.SIG_DFL)
+  signal.signal(signal.SIGINT, signal.SIG_DFL)
   
   # Clean-up
   sc1.botStop()  # stop bot, if moving
